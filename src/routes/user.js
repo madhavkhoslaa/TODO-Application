e53f4bdb8681cc06045b2a586733f6107df191b5
Madhav Khosla <madhav.khoslaa@gmail.com>
2020-07-01 01:02:16 +0530

    /

--- src/routes/user.js
+++ src/routes/user.js
@@ -4,12 +4,23 @@ const User = require('../models/user')
 const userrouter = new express.Router()
 
 userrouter.post('/users', async(req, res) => {
-    user = new User(req.body)
     try {
+        user = new User(req.body)
         await user.save()
         res.status(201).send({ message: "user created successfully" })
     } catch (e) {
         res.status(400).send(e)
+    }
+})
+
+userrouter.get('/users/login', async(req, res) => {
+    try {
+        const user = await User.findbyCredentials(req.body.email, req.body.password)
+        if (!user) return res.status(400).send('unable to login')
+        const token = await user.getAuthtoken()
+        res.status(200).send({ user, token })
+    } catch (e) {
+        res.status(400).send("unable to login")
     }
 })
 
@@ -37,7 +48,9 @@ userrouter.patch('/users/:id', async(req, res) => {
     const validrequest = updates.every((update) => validkeys.includes(update))
     if (!validrequest) return res.status(400).send({ error: "bad request" })
     try {
-        const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
+        const user = await User.findById(req.params.id)
+        updates.forEach(update => user[update] = req.body[update])
+        await user.save()
         if (!user) return res.status(404).send({ error: `user with ${req.params.id} not found` })
         res.status(200).send(user)
     } catch (e) {
