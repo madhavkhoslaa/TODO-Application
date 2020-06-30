92136dd953d1d0862faa1fe872adb77dfb7b893b
Madhav Khosla <madhav.khoslaa@gmail.com>
2020-07-01 01:00:18 +0530

    database connection URI

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,3 +1,3 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://localhost:27017/test-api', { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
+mongoose.connect('mongodb://127.0.0.1:27017/test-api', { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
