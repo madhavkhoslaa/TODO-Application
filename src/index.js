0a4eb2751ea381a423c2758389076318943738c6
Madhav Khosla <madhav.khoslaa@gmail.com>
2020-07-02 05:36:11 +0530

    included middleware and used it

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ const express = require('express')
 
 const UserRouter = require('./routes/user')
 const TaskRouter = require('./routes/task')
+const auth = require('./middleware/auth')
 require('./db/mongoose')
 
 
 const port = process.env.port || 7001
 
 const app = express()
+app.use(auth)
 app.use(express.json())
 app.use(UserRouter)
 app.use(TaskRouter)
