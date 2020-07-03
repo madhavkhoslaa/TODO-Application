6a0ac8a6147d8191ae0aa2e0e58550042c34c46a
Madhav Khosla <madhav.khoslaa@gmail.com>
2020-07-03 20:28:11 +0530

    authentication midlware

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -5,10 +5,16 @@ const User = require("../models/user")
 
 const auth = async(req, res, next) => {
     try {
-        console.log("middle ware")
+        const Authtoken = req.header("Authorization").replace("Bearer", "").trim()
+        const decoded = jwt.verify(Authtoken, "password")
+        const user_ = User.findOne({ _id: decoded._id, 'tokens.token': Authtoken }, { lean: true })
+        if (!user_) throw new Error()
+        req.user = user_._doc
+        console.log(user_)
         next()
     } catch (e) {
         console.log(e)
+        res.status(200).send("unable to authorize")
     }
 
 }
