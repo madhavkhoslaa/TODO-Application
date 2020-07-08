9f6596d1268c530b5f7a9dc215a8e20401477835
Madhav Khosla <madhav.khoslaa@gmail.com>
2020-07-08 12:51:22 +0530

    Sending 0th element of the userdata so that its a json obj instead of json collection

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -7,16 +7,13 @@ const auth = async(req, res, next) => {
     try {
         const Authtoken = req.header("Authorization").replace("Bearer", "").trim()
         const decoded = jwt.verify(Authtoken, "password")
-        const user_ = User.findOne({ _id: decoded._id, 'tokens.token': Authtoken }, { lean: true })
-        if (!user_) throw new Error()
-        req.user = user_._doc
-        console.log(user_)
+        const user_ = await User.find({ _id: decoded._id, 'tokens.token': Authtoken })
+        req.user = user_[0]
+        req.token = Authtoken
         next()
     } catch (e) {
-        console.log(e)
-        res.status(200).send("unable to authorize")
+        res.status(401).send({ error: "unable to authorize" })
     }
-
 }
 
 module.exports = auth
