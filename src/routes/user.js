b05138940625900ddc74af2067d2ebf7dcb83279
Madhav Khosla <madhav.khoslaa@gmail.com>
2020-07-08 12:52:20 +0530

    removed redundant endpoints and cleaned code

--- src/routes/user.js
+++ src/routes/user.js
@@ -5,9 +5,11 @@ const auth = require('../middleware/auth')
 const userrouter = new express.Router()
 
 userrouter.post('/users', async(req, res) => {
+    /*This endpoint is used to accept a user */
     try {
         user = new User(req.body)
         const token = await user.getAuthtoken()
+        console.log(user)
         await user.save()
         res.status(201).send({ user, token })
     } catch (e) {
@@ -16,9 +18,9 @@ userrouter.post('/users', async(req, res) => {
 })
 
 userrouter.get('/users/login', async(req, res) => {
+    /*User login endpoint, accepts email ID and password */
     try {
         const user = await User.findbyCredentials(req.body.email, req.body.password)
-        console.log(user)
         if (!user) {
             return res.status(400).send('unable to login')
         }
@@ -29,34 +31,34 @@ userrouter.get('/users/login', async(req, res) => {
         res.status(400).send("unable to login")
     }
 })
-
-userrouter.get('/users/:id', auth, async(req, res) => {
+userrouter.post('/users/logout', auth, async(req, res) => {
+    /* User logout with JWT token that is stored locally*/
     try {
-        const user = await User.findById(req.params.id)
-        res.status(200).send(user)
+        req.user.tokens = req.user.tokens.filter((token) => token.token != req.token)
+        await req.user.save()
+        res.status(200).send({ message: "user logged out" })
+    } catch (e) {
+        res.status(500).send("internal server error")
+    }
+})
+
+userrouter.get('/users/me', auth, async(req, res) => {
+    try {
+        res.status(200).send(req.user)
     } catch (e) {
         res.status(400).send({ error: "not found" })
     }
 })
 
-userrouter.get('/users', auth, async(req, res) => {
-    try {
-        const users = await User.find({})
-        res.status(200).send(users)
-    } catch (e) {
-        res.status(400).send({ error: "not found" })
-    }
-})
-
-userrouter.patch('/users/:id', async(req, res) => {
+userrouter.patch('/users/:id', auth, async(req, res) => {
     const updates = Object.keys(req.body)
     const validkeys = ['name', 'email', 'password', 'age']
     const validrequest = updates.every((update) => validkeys.includes(update))
     if (!validrequest) return res.status(400).send({ error: "bad request" })
     try {
-        const user = await User.findById(req.params.id)
+        const user = req.user
         updates.forEach(update => user[update] = req.body[update])
-        await user.save()
+        await req.user.save()
         if (!user) return res.status(404).send({ error: `user with ${req.params.id} not found` })
         res.status(200).send(user)
     } catch (e) {
@@ -64,14 +66,14 @@ userrouter.patch('/users/:id', async(req, res) => {
     }
 })
 
-userrouter.delete('/users/:id', async(req, res) => {
+userrouter.delete('/users/me', auth, async(req, res) => {
     const id_ = req.params.id
     try {
-        const user = await User.findByIdAndDelete(id_)
-        if (!user) return res.status(404).send({ error: `user with id: ${id_} not found` })
+        req.user.delete()
         res.status(400).send({ message: "user deleted" })
     } catch (e) {
         res.status(500).send({ error: "internal server error" })
     }
 })
+
 module.exports = userrouter
